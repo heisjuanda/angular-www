06ed8987056213758347ebf1353dcdb4a8523974
Juan David Moreno Alfonso <juandavid@cressco.dev>
2024-05-08 09:53:10 -0500

    add: practica 6

--- src/componente/heroes/add-hero/add-hero.component.ts
+++ src/componente/heroes/add-hero/add-hero.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpRequestService } from '../../../services/http-request.service';
 
 @Component({
   selector: 'app-add-hero',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './add-hero.component.html',
   styleUrl: './add-hero.component.css',
+  providers: [HttpRequestService],
 })
 export class AddHeroComponent {
+  constructor(private _http_service: HttpRequestService) {}
+
+  public myService(): any {
+    this._http_service
+      .postRequestWithParams('Yilmar', 'yilmar.alzate@correounivalle.edu.co')
+      .subscribe((data: any) => {
+        console.log(`Data en la peticion: `, data);
+      });
+  }
+
   public heroe: any = {
     nombre: 'My Hero!!',
     poder: 2,
